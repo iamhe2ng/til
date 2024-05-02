13684371f6851787569a478b508fe24406939ad4
lily <lily@inexkr.com>
2024-05-02 14:54:38 +0900

    Bootstrap 적용

--- nodejs-api/index.js
+++ nodejs-api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use('/static', express.static(__dirname + '/node_modules'));
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
